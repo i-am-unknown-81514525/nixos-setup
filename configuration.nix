03efb5d0d88352324f4b10367ee12706779eade4
i-am-unknown-81514525 <74453352+i-am-unknown-81514525@users.noreply.github.com>
2026-01-26 21:05:40 +0000

    Add custom fontconfig for Noto Color Emoji

--- configuration.nix
+++ configuration.nix
@@ -112,4 +112,22 @@
       emoji = [ "Noto Color Emoji" ];
     };
   };
+  fonts.fontconfig.localConf = ''
+  <?xml version="1.0"?>
+  <!DOCTYPE fontconfig SYSTEM "fonts.dtd">
+  <fontconfig>
+    <alias>
+      <family>serif</family>
+      <prefer><family>Noto Color Emoji</family></prefer>
+    </alias>
+    <alias>
+      <family>sans-serif</family>
+      <prefer><family>Noto Color Emoji</family></prefer>
+    </alias>
+    <alias>
+      <family>monospace</family>
+      <prefer><family>Noto Color Emoji</family></prefer>
+    </alias>
+  </fontconfig>
+'';
 }
